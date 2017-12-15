d84f22e82eb819a585c27ed554adabdf5aa17f76
Joseph Long <github@joseph-long.com>
2017-12-15 13:35:31 -0700

    Who knows what

--- doodads/__init__.py
+++ doodads/__init__.py
@@ -26,8 +26,52 @@ from astropy.convolution import convolve_fft
 from astropy.convolution.kernels import Gaussian2DKernel
 from photutils import centroid_com, centroid_1dg, centroid_2dg
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+def add_colorbar(mappable):
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    return fig.colorbar(mappable, cax=cax)
+
+import matplotlib.gridspec as gridspec
+from itertools import product
+
+@supply_argument(fig=lambda: plt.gcf())
+def image_grid(cube, columns, colorbar=False, cmap=None, fig=None):
+    rows = (
+        cube.shape[0] // columns
+        if cube.shape[0] % columns == 0
+        else cube.shape[0] // columns + 1
+    )
+    gs = gridspec.GridSpec(rows, columns)
+    for idx, (row, col) in enumerate(product(range(rows), range(columns))):
+        ax = fig.add_subplot(gs[row, col])
+        im = ax.imshow(cube[idx], cmap=cmap)
+        if colorbar:
+            add_colorbar(im)
+    return fig
+
+@supply_argument(ax=lambda: plt.gca())
+def show_diff(im1, im2, ax=None, vmax=None, cmap=matplotlib.cm.RdBu_r, scale='linear'):
+    diff = im1 - im2
+    if vmax is not None:
+        clim = vmax
+    else:
+        clim = np.nanmax(np.abs(diff))
+    if scale == 'linear':
+        im = ax.imshow(diff, vmin=-clim, vmax=clim, cmap=cmap)
+    elif scale == 'log':
+        im = ax.imshow(diff, vmin=-clim, vmax=clim, cmap=cmap, norm=LogNorm())
+    else:
+        raise ValueError("Unknown scale: {}".format(scale))
+    return im
+
+
 class CoupledCutout:
     LEAK_BOX_SIZE = 45
+    PSF_BOX_SIZE = 180
     SMOOTH_WIDTH_PX = 3
     INITIAL_GUESS_LEAK_CENTER = 250, 287
     PSF_OFFSET = -60, 130
@@ -40,9 +84,12 @@ class CoupledCutout:
             self.center_leak = self.INITIAL_GUESS_LEAK_CENTER
 
         if refine:
-            self.center_leak = self._centroid_leak(verbose=verbose)
-        if verbose:
-            print("Leak term center:", self.center_leak)
+            self.center_leak = self._centroid(
+                initial_guess=self.center_leak,
+                data=self.data,
+                size=self.LEAK_BOX_SIZE,
+                verbose=verbose
+            )
 
         leak_x, leak_y = self.center_leak
         offset_x, offset_y = self.PSF_OFFSET
@@ -50,52 +97,90 @@ class CoupledCutout:
             self.center_top = center_top
         else:
             self.center_top = leak_x + offset_x, leak_y + offset_y
-
-        if refine:
-            pass
-        if verbose:
-            print("Top PSF center:", self.center_top)
-
         if center_bottom is not None:
             self.center_bottom = center_bottom
         else:
             self.center_bottom = leak_x - offset_x, leak_y - offset_y
 
         if refine:
-            pass
-
-    def _centroid_leak(self, verbose=False):
-        cutout = Cutout2D(
+            self.center_top = self._centroid(
+                initial_guess=self.center_top,
+                data=self.data,
+                size=self.PSF_BOX_SIZE,
+                verbose=verbose
+            )
+            self.center_bottom = self._centroid(
+                initial_guess=self.center_bottom,
+                data=self.data,
+                size=self.PSF_BOX_SIZE,
+                verbose=verbose
+            )
+        if verbose:
+            print("Leak term center:", self.center_leak)
+            print("Top PSF center:", self.center_top)
+            print("Bottom PSF center:", self.center_bottom)
+        self.top_cutout = Cutout2D(
             self.data,
-            copy=True,
+            position=self.center_top,
+            size=self.PSF_BOX_SIZE,
+        )
+        self.bottom_cutout = Cutout2D(
+            self.data,
+            position=self.center_bottom,
+            size=self.PSF_BOX_SIZE,
+        )
+        self.leak_cutout = Cutout2D(
+            self.data,
             position=self.center_leak,
             size=self.LEAK_BOX_SIZE,
         )
+    @classmethod
+    def _centroid(cls, initial_guess, data, size, verbose=False):
+        cutout = Cutout2D(
+            data,
+            copy=True,
+            position=initial_guess,
+            size=size,
+        )
         cutout.data = convolve_fft(
             cutout.data,
-            Gaussian2DKernel(self.SMOOTH_WIDTH_PX),
+            Gaussian2DKernel(cls.SMOOTH_WIDTH_PX),
             boundary='wrap'
         )
+        centroid = centroid_1dg(cutout.data)
         if verbose:
-            fig, ax = plt.subplots()
-            ax.imshow(cutout.data)
-            ax.scatter(*centroid_com(cutout.data), label="COM")
-            ax.scatter(*centroid_1dg(cutout.data), label="1D G.")
-            ax.scatter(*centroid_2dg(cutout.data), label="2D G.")
-            ax.legend()
-        return cutout.to_original_position(centroid_2dg(cutout.data))
+            #fig, ax = plt.subplots()
+            #ax.imshow(cutout.data)
+            #ax.scatter(*centroid_com(cutout.data), label="COM")
+            #ax.scatter(*centroid_1dg(cutout.data), label="1D G.")
+            #ax.scatter(*centroid_2dg(cutout.data), label="2D G.")
+            #ax.legend()
+            print("Centroid:", centroid)
+        return cutout.to_original_position(centroid)
 
     @supply_argument(ax=lambda: plt.gca())
-    def display(self, ax=None):
-        ax.imshow(self.data, norm=LogNorm())
+    def display(self, ax=None, colorbar=False, vmin=None, vmax=None):
+        img = ax.imshow(self.data, norm=LogNorm(), vmin=vmin, vmax=vmax)
+        add_colorbar(img)
         ax.add_artist(plt.Circle(
             self.center_leak,
             radius=self.LEAK_BOX_SIZE / 2,
             facecolor="none",
             edgecolor="C0"
         ))
-        ax.plot(*list(zip(self.center_leak, self.center_top)))
-        ax.plot(*list(zip(self.center_leak, self.center_bottom)))
+        for loc in (self.center_top, self.center_bottom):
+            ax.add_artist(plt.Rectangle(
+                xy=(
+                    loc[0] - self.PSF_BOX_SIZE / 2,
+                    loc[1] - self.PSF_BOX_SIZE / 2
+                ),
+                width=self.PSF_BOX_SIZE,
+                height=self.PSF_BOX_SIZE,
+                fc='none',
+                ec='C1'
+            ))
+            ax.plot(*list(zip(self.center_leak, loc)))
+        return ax
 
 def rotated_sigmoid_2d(x, y, maximum=1, theta=0, x_0=0, y_0=0, k=1):
     xs = x - x_0
