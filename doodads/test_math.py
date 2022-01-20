4cb0f2ec43cdadec65632305986e780be078416b
Joseph Long <github@joseph-long.com>
2022-01-19 22:27:37 -0700

    math: unitful make_monotonic

--- doodads/test_math.py
+++ doodads/test_math.py
@@ -1,5 +1,6 @@
 import numpy as np
-from .math import modified_gram_schmidt
+import astropy.units as u
+from .math import modified_gram_schmidt, make_monotonic_decreasing, make_monotonic_increasing
 
 def test_modified_gram_schmidt():
     init_x = np.asarray([
@@ -15,3 +16,31 @@ def test_modified_gram_schmidt():
         [0, 0, 0]
     ])
     assert np.allclose(result, ref), "Last column should be zeroed by MGS"
+
+
+
+def goofy_function(x):
+    # construct a function with inflection points
+    # at x = 1 and x = 2
+
+    # first we want d/dx(f(x)) to have zeros at
+    # those coordinates:
+    # d/dx(f(x)) = (x - 1) * (x - 2)
+    #            = x**2 - 3 * x + 2
+    # then we get f(x) by integrating, choosing
+    # a constant of 0
+    return x**3 / 3 - 3 * x**2 / 2 + 2 * x
+
+def test_make_monotonic():
+    xs = np.linspace(0, 3)
+    new_xs, new_ys = make_monotonic_increasing(xs, goofy_function(xs))
+    assert not np.any(new_ys[new_xs > 1] < goofy_function(1))
+    new_xs, new_ys = make_monotonic_decreasing(xs, -goofy_function(xs))
+    assert not np.any(new_ys[new_xs > 1] > goofy_function(1))
+
+def test_make_monotonic_units():
+    xs = np.linspace(0, 3) * u.m
+    ys = xs**2
+    new_xs, new_ys = make_monotonic_increasing(xs, ys)
+    assert new_xs.unit == xs.unit
+    assert new_ys.unit == ys.unit
